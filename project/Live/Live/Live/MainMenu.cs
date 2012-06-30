625f457f034488012bedef71e2b97c7c5e3aa383
Eugene Dementjev <daemos@guitarplayer.ru>
2012-06-30 14:28:06 +0400

    main menu and 2 backgrounds for mainmenu

---
+++ project/Live/Live/Live/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Live
+{
+    class MainMenu
+    {
+        Texture2D background;
+        List<TextButton> buttons;
+
+    }
+}
